05cc9baa3064074d8f9dcaeae7aac28bb04d0e32
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-15 21:43:07 -0500

    Create every_other_tweet.py

---
+++ every_other_tweet.py
@@ -0,0 +1,27 @@
+import main
+import json
+import os
+
+x = main.make_token()
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+token_url = "https://api.twitter.com/2/oauth2/token"
+
+t = main.r.get("token")
+bb_t = t.decode("utf8").replace("'", '"')
+data = json.loads(bb_t)
+
+refreshed_token = x.refresh_token(
+    client_id=client_id,
+    client_secret=client_secret,
+    token_url=token_url,
+    refresh_token=data["refresh_token"],
+)
+
+st_refreshed_token = '"{}"'.format(refreshed_token)
+j_refreshed_token = json.loads(st_refreshed_token)
+main.r.set("token", j_refreshed_token)
+
+content = main.parse_post_content()
+payload = {"text": "{}".format(content)}
+main.post_tweet(payload, refreshed_token)
