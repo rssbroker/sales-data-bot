b0f702964deb9874c3ec815da955f8c2f1008ca6
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-17 05:27:01 -0500

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -8,11 +8,11 @@ from selenium.webdriver.support import expected_conditions as EC
 def get_url(website_url, email, password):
     received_url = ''
     options = webdriver.ChromeOptions()
-    options.binary_location('/opt/render/project/.render/chrome/opt/google/chrome')
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
     options.add_argument('--headless')
     options.add_argument('--remote-debugging-pipe')
+    options.binary_location = '/opt/render/project/.render/chrome/opt/google/chrome'
     driver = webdriver.Chrome(options=options)
     
     try:
