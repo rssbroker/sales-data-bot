21da2bbfd95e963ef6ffcf8658eb9c7aaa445056
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-17 02:04:44 -0500

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,8 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def get_url(website_url, email, password):
     received_url = ''
