443e6e57ddc17d18b914d031e45bb6ebca1c2342
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-16 19:27:09 -0500

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,30 +6,36 @@ from selenium.common.exceptions import NoSuchElementException
 
 def get_url(website_url, email, password):
     received_url = ''
+    
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--incognito')
     options.add_argument('--headless')
-    driver = webdriver.Chrome(
-        "/usr/lib/chromium-browser/chromedriver", chrome_options=options
-    )
-    driver.get(website_url)
-    time.sleep(2)
+    
+    driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+    
     try:
-        member_button = driver.find_element(By.LINK_TEXT, "Member Login")
+        driver.get(website_url)
+        wait = WebDriverWait(driver, 10)
+        
+        # Wait for the Member Login link to be present
+        member_button = wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Member Login")))
+        
+        # If the login link is present, perform login
+        member_button.click()
+        wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='email']"))).send_keys(email)
+        wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='password']"))).send_keys(password)
+        wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div[2]/div/div[3]/button"))).click()
+        
+        # Wait for the page to load after login
+        wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='some-class']")))
+        
+        # Get the URL after login
+        received_url = driver.page_source
+    
     except NoSuchElementException:
         received_url = driver.page_source
-    else:
-        member_button.click()
-        time.sleep(2)
-        driver.find_element(
-            By.XPATH, "//*[@id='email']").send_keys(email)
-        driver.find_element(
-            By.XPATH, "//*[@id='password']").send_keys(password)
-        driver.find_element(
-            By.XPATH, "/html/body/div[7]/div[2]/div/div[3]/button").click()
-        time.sleep(2)
-        received_url = driver.page_source
+    
     finally:
         driver.quit()
         return received_url
