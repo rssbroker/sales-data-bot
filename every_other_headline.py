7fad73f7f59b7135e899497912e0fe477566ed36
rssbroker <153944461+rssbroker@users.noreply.github.com>
2024-02-29 16:13:48 -0500

    Create every_other_headline.py

---
+++ every_other_headline.py
@@ -0,0 +1,31 @@
+import main
+import json
+import os
+
+headlines = main.r.get('headlines')
+if headlines:
+    headlines = json.loads(headlines)
+    if len(headlines) > 0:
+        x = main.make_token()
+        client_id = os.environ.get("CLIENT_ID")
+        client_secret = os.environ.get("CLIENT_SECRET")
+        token_url = "https://api.twitter.com/2/oauth2/token"
+
+        t = main.r.get("token")
+        bb_t = t.decode("utf8").replace("'", '"')
+        data = json.loads(bb_t)
+
+        refreshed_token = x.refresh_token(
+            client_id=client_id,
+            client_secret=client_secret,
+            token_url=token_url,
+            refresh_token=data["refresh_token"],
+        )
+
+        st_refreshed_token = '"{}"'.format(refreshed_token)
+        j_refreshed_token = json.loads(st_refreshed_token)
+        main.r.set("token", j_refreshed_token)
+
+        content = main.get_headline()
+        payload = {"text": "{}".format(content)}
+        main.post_tweet(payload, refreshed_token)
